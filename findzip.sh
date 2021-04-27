ad110b9bdb2ca578aa912bdb38fd6e8443537600
root <salehstudent@gmail.com>
2021-04-27 14:01:05 +0430

    find inside zip file

---
+++ findzip.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    
+    echo " use findzip -k(keyword) -p(path_file) -f(format)"
+    
+    exit 1
+fi
+while  getopts k:p:f: flag 
+do 
+   case "${flag}" in 
+        k) keyword=${OPTARG};;
+    
+        p) path_file=${OPTARG};;
+    
+        f) format=${OPTARG};;
+   
+   esac
+done 
+ 
+ for x in `find $path_file -name *$format`;
+    
+       do   value=`unzip -p  $x 2>/dev/null`  
+     
+          machted_str=`echo $value | grep -c "$keyword"`  
+     
+          if [[ $machted_str == 1 ]] ; then 
+           
+          
+            echo "matched in $x"
+          
+         fi 
+ done
